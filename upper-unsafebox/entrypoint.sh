6155ca129cae310b2f3ea22c07d239f1b8fca765
José Nieto <jose@xiam.io>
2019-11-04 18:00:41 +0000

    remove commented out iptales rules

--- upper-unsafebox/entrypoint.sh
+++ upper-unsafebox/entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-#iptables -P INPUT DROP
-#iptables -P FORWARD DROP
-#iptables -P OUTPUT DROP
-
-#iptables -A INPUT -i eth0 -p tcp --dport 8080 -m state --state NEW,ESTABLISHED -j ACCEPT
-#iptables -A OUTPUT -o eth0 -p tcp --sport 8080 -m state --state ESTABLISHED -j ACCEPT
-
-#iptables -A OUTPUT -o eth0 -p tcp -m tcp --syn -j REJECT --reject-with icmp-host-prohibited
-
 rm -rf $WORKDIR/c
 
 mkdir -p $WORKDIR/c/bin
