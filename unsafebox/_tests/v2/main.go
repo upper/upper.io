b09bf32f5fe612fd2c27402b1b4d39abb5d748ce
José Nieto <jose@xiam.io>
2020-08-25 14:55:21 -0500

    update versions

--- unsafebox/_tests/v2/main.go
+++ unsafebox/_tests/v2/main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	_ "upper.io/db.v2"
+	_ "upper.io/db.v2/mongo"
+	_ "upper.io/db.v2/mysql"
+	_ "upper.io/db.v2/postgresql"
+	_ "upper.io/db.v2/ql"
+	_ "upper.io/db.v2/sqlite"
 )
 
 func main() {
