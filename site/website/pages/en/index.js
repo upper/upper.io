6edd82e5f2a90fa803e661cfdd29c3e47dab1032
José Nieto <jose@xiam.io>
2020-08-27 15:18:02 -0500

    update tour button

--- site/website/pages/en/index.js
+++ site/website/pages/en/index.js
@@ -64,7 +64,7 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
           <PromoSection>
-            <Button href="//tour.dev.upper.io" target="_blank">Take the tour</Button>
+            <Button href="//tour.upper.io" target="_blank">Take the tour</Button>
           </PromoSection>
         </div>
       </SplashContainer>
